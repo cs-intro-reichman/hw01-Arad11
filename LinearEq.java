8b13780a2252abbf3bfdda2dda069c0dfe909960
Arad Koluok <arad.koluok@runi.ac.il>
2023-12-19 00:01:31 +0200

    LinearEq works

--- LinearEq.java
+++ LinearEq.java
@@ -5,5 +5,12 @@
  * Treats the three arguments as well as the computed value as double values
  */
 public class LinearEq {
-	// Put your code here
+	public static void main(String args[]){
+		double a = Double.parseDouble(args[0]);
+		double b = Double.parseDouble(args[1]);
+		double y = Double.parseDouble(args[2]);
+		double x = (y-b)/a;
+		System.out.println(a+" * x + " + b + " = " + y);
+		System.out.println("X = " + x);
+	}	
 }
