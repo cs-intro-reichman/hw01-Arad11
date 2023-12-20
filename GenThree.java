6b95dfc4a1720d9b4d77608eff4be3b032f2eb16
Arad Koluok <arad.koluok@post.runi.ac.il>
2023-12-21 00:39:05 +0200

    validate which one is smaller

--- GenThree.java
+++ GenThree.java
@@ -10,13 +10,18 @@ public class GenThree {
 		Random ran = new Random();
 		int buttom = Integer.parseInt(args[0]);
 		int top = Integer.parseInt(args[1]);
-		int firstNum = ran.nextInt(buttom,top);
-		int secondNum = ran.nextInt(buttom,top);
-		int thirdNum = ran.nextInt(buttom,top);
-		int minNum = Math.min(Math.min(firstNum,secondNum), thirdNum);
+		if ( top < buttom ) {
+			int temp = top;
+			top = buttom;
+			buttom = temp;
+		}
+		int firstNum = ran.nextInt(buttom, top);
+		int secondNum = ran.nextInt(buttom, top);
+		int thirdNum = ran.nextInt(buttom, top);
+		int minNum = Math.min(Math.min(firstNum, secondNum), thirdNum);
 		System.out.println(firstNum);
 		System.out.println(secondNum);
 		System.out.println(thirdNum);
-		System.out.println("the minimal generated number was "+ minNum);
+		System.out.println("the minimal generated number was " + minNum);
 	}
 }
