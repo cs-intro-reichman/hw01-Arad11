a68feb259f5c8267aa918c133e6a18f2144b250c
Arad Koluok <arad.koluok@post.runi.ac.il>
2023-12-21 22:33:03 +0200

    typo

--- AddTwo.java
+++ AddTwo.java
@@ -4,7 +4,7 @@
 public class AddTwo {
 	public static void main(String[] args) {
 		int firstNumber = Integer.parseInt(args[0]);
-		int secoundArg = Integer.parseInt(args[1]);
-		System.out.println(args[0] + " + " + args[1] + " = "+ (firstNumber + secoundArg));
+		int secondArg = Integer.parseInt(args[1]);
+		System.out.println(args[0] + " + " + args[1] + " = "+ (firstNumber + secondArg));
 	}
 }
