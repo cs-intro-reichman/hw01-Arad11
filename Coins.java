99116993aa48dbfcbbb11b93dc9ecef19b38cbf4
Arad Koluok <arad.koluok@post.runi.ac.il>
2023-12-21 22:12:08 +0200

    quarter as a final

--- Coins.java
+++ Coins.java
@@ -4,9 +4,10 @@
  */
 public class Coins {
 	public static void main(String[] args) {
+		final int QUARTER_EQUALITY = 25;
 		int coinsAmount = Integer.parseInt(args[0]);
-		int quartersAmount = coinsAmount / 25;
-		int centsAmount = coinsAmount % 25;
+		int quartersAmount = coinsAmount / QUARTER_EQUALITY;
+		int centsAmount = coinsAmount % QUARTER_EQUALITY;
 
 		System.out.println("Use " + quartersAmount + " quarters and " + centsAmount + " cents");
 	}
